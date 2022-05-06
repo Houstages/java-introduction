a8273c126e86b9b5a617e33210c55b05c1446b71
Houstages <xbox.houstage@gmail.com>
2022-05-06 20:02:38 +0300

    Add gitignore file

---
+++ src/Lesson4/Korm.java
@@ -0,0 +1,22 @@
+package Lesson4;
+
+public class Korm {
+    public static void main(String[] args) {
+        int cats = 8, dogs = 24, porcia = 50, price = 2;
+
+        int kormCats = kormMonth(cats, porcia);
+        int kormDogs = kormMonth(dogs, porcia);
+        int money = moneyMonth(kormCats, kormDogs, price);
+
+        System.out.println("Нужно корма для котов в месяц: " + kormCats);
+        System.out.println("Нужно корма для собак в месяц: " + kormDogs);
+        System.out.println("Нужно денег в месяц на содержание приюта: " + money);
+    }
+
+    public static int kormMonth(int a, int b) {
+        return a * b;
+    }
+    public static int moneyMonth(int a, int b, int c) {
+        return (a + b) * c;
+    }
+}
