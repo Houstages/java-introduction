beff2c7af2bcf6569cfd94f056a28311925bce51
Houstages <xbox.houstage@gmail.com>
2022-05-08 21:11:51 +0300

    Add gitignore file

--- src/Lesson4/Korm.java
+++ src/Lesson4/Korm.java
@@ -2,19 +2,19 @@ package Lesson4;
 
 public class Korm {
     public static void main(String[] args) {
-        int cats = 8, dogs = 24, portion = 50, price = 2;
+        int cats = 8, dogs = 24, portion = 50, pricePerServing = 2;
 
         int kormCats = kormMonth(cats, portion);
         int kormDogs = kormMonth(dogs, portion);
-        int money = moneyMonth(kormCats, kormDogs, price);
+        int money = moneyMonth(kormCats, kormDogs, pricePerServing);
 
-        System.out.println("Нужно корма для котов в месяц: " + kormCats);
-        System.out.println("Нужно корма для собак в месяц: " + kormDogs);
-        System.out.println("Нужно денег в месяц на содержание приюта: " + money);
+        System.out.println("РќСѓР¶РЅРѕ РєРѕСЂРјР° РґР»СЏ РєРѕС‚РѕРІ РІ РјРµСЃСЏС†: " + kormCats);
+        System.out.println("РќСѓР¶РЅРѕ РєРѕСЂРјР° РґР»СЏ СЃРѕР±Р°Рє РІ РјРµСЃСЏС†: " + kormDogs);
+        System.out.println("РќСѓР¶РЅРѕ РґРµРЅРµРі РІ РјРµСЃСЏС† РЅР° СЃРѕРґРµСЂР¶Р°РЅРёРµ РїСЂРёСЋС‚Р°: " + money);
     }
 
-    public static int kormMonth(int a, int b) {
-        return ((a * b) * 3) * 30;
+    public static int kormMonth(int pet, int portion) {
+        return ((pet * portion) * 3) * 30;
     }
     public static int moneyMonth(int a, int b, int c) {
         return (a + b) * c;
