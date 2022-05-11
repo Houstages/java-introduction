f3c84c45e4db6951466d0d88fc8a156db04d7bfb
Houstages <xbox.houstage@gmail.com>
2022-05-11 23:49:25 +0300

    Add gitignore file

--- src/Lesson7/UnluckyNumbers.java
+++ src/Lesson7/UnluckyNumbers.java
@@ -4,13 +4,23 @@ public class UnluckyNumbers {
     public static void main(String[] args) {
 
         System.out.print("Lucky numbers: ");
-        for(int number = 0, unluck = 4; number <= 100; number++){
-            if (number != unluck){
+        for(int number = 0, unluck1 = 4, unluck2 = 40, i = 49; number <= 100; number++){
+            if (number != unluck1 && number != unluck2){
                 System.out.print(number + ", ");
             }
-            else{
-                unluck = unluck + 5;
+            else if (number == unluck2) {
+                while (unluck2 < i) {
+                    unluck2++;
+                    number++;
+                    }
+                i = i + 51;
+                unluck1 = unluck1 + 10;
+                unluck2 = unluck2 + 41;
+            }
+            else {
+                unluck1 = unluck1 + 5;
+                }
             }
         }
     }
-}
+
