b418aac6da84eb501022d475e9989fc2a6c9c313
Houstages <xbox.houstage@gmail.com>
2022-05-20 18:40:24 +0300

    Add gitignore file

---
+++ src/Lesson8/Palindrome.java
@@ -0,0 +1,21 @@
+/*package Lesson8;
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введіть розмір масиву: ");
+        int n = in.nextInt();
+
+        String[] array = new String[];
+        System.out.print("Введіть строку: ");
+        String sentence = in.next();
+
+        array = sentence.split();
+
+
+        for(int i = 0; i < n; i++)  System.out.println(array[i]);
+    }
+}
+*/
