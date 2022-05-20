68efa4026ea43e44c15911b0b3329460ea8537a7
Houstages <xbox.houstage@gmail.com>
2022-05-20 19:06:31 +0300

    Add gitignore file

--- src/Lesson8/Palindrome.java
+++ src/Lesson8/Palindrome.java
@@ -1,21 +1,26 @@
-/*package Lesson8;
+package Lesson8;
 import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Введіть розмір масиву: ");
-        int n = in.nextInt();
-
-        String[] array = new String[];
         System.out.print("Введіть строку: ");
         String sentence = in.next();
+        char[] array = sentence.toCharArray();
 
-        array = sentence.split();
+        int k = array.length-1;
 
-
-        for(int i = 0; i < n; i++)  System.out.println(array[i]);
+        for(int i = 0; i < array.length; i++, k--){
+            char x1 = array[i];
+            char x2 = array[k];
+            if(x1 != x2){
+                System.out.println("Не є паліндромом");
+                break;
+            }
+            else if (k == 0) {
+                System.out.println("Паліндром");
+            }
+        }
     }
 }
-*/
